1bac50c8a9995ca00b4a7f821795db3404c0cdd5
HasanovaElgul <98238310+HasanovaElgul@users.noreply.github.com>
2024-08-28 23:39:01 +0400

    home

---
+++ BIZNEWS FREE/ViewComponents/HomeCategoryViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using BIZNEWS_FREE.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BIZNEWS_FREE.ViewComponents
+{
+    public class HomeCategoryViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public HomeCategoryViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+
+
+        {
+
+            var categories = await _context.Categories.Include(x => x.Article).ToListAsync();
+            return View("HomeCategory", categories);
+
+        }
+    }
+}
